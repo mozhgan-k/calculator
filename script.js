3801c769b45c4434e269b0ef41423ec4c9f6d34c
Mozhgan <mozhan.kabki@gmail.com>
2023-09-14 15:11:15 +0330

    add keyboard event

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const res = document.getElementById("result")
 const list = document.getElementById("keys-list")
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const operators = ["×", "-", "+", "+/-", "="]
 
 for (const number of numbers) {
@@ -10,7 +10,7 @@ for (const number of numbers) {
   button.classList.add("keys", "bg-body")
   button.innerText = number
   button.addEventListener("click", function liveScreen() {
-    if(typeof res.value !== NaN) {
+    if (typeof res.value !== NaN) {
       res.value += number
     }
   })
@@ -18,16 +18,20 @@ for (const number of numbers) {
   list.appendChild(li)
 }
 
+function liveScreen(value) {
+  if (typeof res.value !== NaN) {
+    res.value += value
+  }
+}
+
 function calculate(value) {
-  const calculatedValue = eval(value || null)
-  console.log(calculatedValue)
-  if (isNaN(calculatedValue)) {
+  if (isNaN(value)) {
     res.value = "Can not divide 0 with 0"
     setTimeout(() => {
       res.value = "";
     }, 1300);
   } else {
-    res.value = calculatedValue
+    res.value = value
   }
 }
 
@@ -36,65 +40,50 @@ function backspace() {
   res.value = result.substring(0, res.value.length - 1)
 }
 
+//Add keyboard events
+
 document.addEventListener("keydown", keyboardEventHandler)
 
 function keyboardEventHandler(e) {
-  console.log(e.key)
+  console.log(e)
   e.preventDefault();
 
-  if (e.key === "0") {
-    res.value += "0";
-  } else if (e.key === "1") {
-    res.value += "1";
-  } else if (e.key === "2") {
-    res.value += "2";
-  } else if (e.key === "3") {
-    res.value += "3";
-  } else if (e.key === "4") {
-    res.value += "4";
-  } else if (e.key === "5") {
-    res.value += "5";
-  } else if (e.key === "6") {
-    res.value += "6";
-  } else if (e.key === "7") {
-    res.value += "7";
-  } else if (e.key === "8") {
-    res.value += "8";
-  } else if (e.key === "9") {
-    console.log(res.value)
-    console.log(e.key)
-    res.value += "9";
-  } else {
-    return
+  for (const num of numbers) {
+    if (e.key === num) {
+      res.value += num
+    }
   }
 
-  if (e.key === "*") {
-    res.value += "×";
-  } else if (e.key += "-") {
-    res.value += "-";
-  } else if (e.key === "+") {
-    res.value += "+";
-  } else if (e.key === "/") {
-    res.value += "/";
-  } else {
-    return
+  for (const sym of operators) {
+    if (e.key === sym) {
+      if (sym === "×") {
+        sym = "*"
+      }
+      res.value += sym
+    }
   }
 
-  if(e.key === ".") {
+
+  if (e.key === ".") {
     res.value += ".";
   } else {
     return
   }
 
-  if(e.key === "Enter") {
-    calculate(res.value);
-  } else {
-    return
+  if (e.key === "Enter") {
+    if (isNaN(res.value)) {
+      res.value = "Can not divide 0 with 0"
+      setTimeout(() => {
+        res.value = "";
+      }, 1300);
+    } else {
+      return res.value
+    }
   }
 
-  if(e.key === "Backspace") {
-    backspace();
-  } else {
-    return
+  if (e.key === "Backspace") {
+    console.log(res.value)
+    const result = res.value
+    res.value = result.substring(0, res.value.length - 1)
   }
 }
